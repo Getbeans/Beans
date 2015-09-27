495ed0ecab306fc361c4797ea782ddfcbd6393aa
Thierry Muller <thierry@getbeans.io>
2015-09-27 17:40:26 +0200

    Added custom header cropping options.

--- lib/init.php
+++ lib/init.php
@@ -55,7 +55,13 @@ function beans_add_theme_support() {
 	add_theme_support( 'post-thumbnails' );
 	add_theme_support( 'automatic-feed-links' );
 	add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption'  ) );
-	add_theme_support( 'custom-header', array( 'header-text' => false ) );
+	add_theme_support( 'custom-header', array(
+		'width' => 2000,
+		'height' => 500,
+		'flex-height' => true,
+		'flex-width' => true,
+		'header-text' => false
+	) );
 
 	// Beans specific.
 	add_theme_support( 'offcanvas-menu' );
