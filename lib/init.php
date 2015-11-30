864567c74ebf2099ebffe55f54d0f8972d84fbd1
Thierry Muller <thierry@getbeans.io>
2015-11-30 08:05:30 +0200

    Updated to version 1.2.3

--- lib/init.php
+++ lib/init.php
@@ -80,7 +80,7 @@ add_action( 'beans_init', 'beans_define_constants' );
 function beans_define_constants() {
 
 	// Define version.
-	define( 'BEANS_VERSION', '1.2.2' );
+	define( 'BEANS_VERSION', '1.2.3' );
 
 	// Define paths.
 	define( 'BEANS_THEME_PATH', wp_normalize_path( trailingslashit( get_template_directory() ) ) );
