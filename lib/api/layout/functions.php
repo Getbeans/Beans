b23aca13264059cb6f5bacddbf50c2d7319f92aa
ThierryA <thierry@getbeans.io>
2016-06-01 22:30:17 +0200

    Fixed rare layout issue if no posts page is assigned.

--- lib/api/layout/functions.php
+++ lib/api/layout/functions.php
@@ -46,8 +46,8 @@ function beans_get_layout() {
 	if ( is_singular() )
 		$layout = beans_get_post_meta( 'beans_layout' );
 
-	elseif ( is_home() )
-		$layout = beans_get_post_meta( 'beans_layout', false, get_option( 'page_for_posts' ) );
+	elseif ( is_home() && ( 0 != ( $posts_page = get_option( 'page_for_posts' ) ) ) )
+		$layout = beans_get_post_meta( 'beans_layout', false, $posts_page );
 
 	elseif ( is_category() || is_tag() || is_tax() )
 		$layout = beans_get_term_meta( 'beans_layout' );
