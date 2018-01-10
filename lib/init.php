6cb7a85c33bb7b9b84b89a0f7625132aa2a78d85
ThierryA <thierry@getbeans.io>
2018-01-10 09:47:47 +0100

    Updated to version 1.4.1

--- lib/init.php
+++ lib/init.php
@@ -14,7 +14,7 @@ add_action( 'beans_init', 'beans_define_constants', -1 );
 function beans_define_constants() {
 
 	// Define version.
-	define( 'BEANS_VERSION', '1.4.0' );
+	define( 'BEANS_VERSION', '1.4.1' );
 
 	// Define paths.
 	if ( ! defined( 'BEANS_THEME_PATH' ) ) {
