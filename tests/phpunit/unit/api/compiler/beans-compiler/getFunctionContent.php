41dac237e40a3d076ac255f8c88cf7709b815c15
Christoph Herr <info@christophherr.com>
2018-01-31 19:26:16 -0500

    Changes to /unit/api/compiler/beans-compiler/getFunctionContent.php

--- tests/phpunit/unit/api/compiler/beans-compiler/getFunctionContent.php
+++ tests/phpunit/unit/api/compiler/beans-compiler/getFunctionContent.php
@@ -30,17 +30,20 @@ class Tests_Beans_Compiler_Get_Function_Content extends Compiler_Test_Case {
 	public function test_should_return_false_when_fragment_not_callable() {
 		$compiler = new \_Beans_Compiler( array() );
 
+		// Run the tests.
 		$this->assertfalse( $compiler->get_function_content() );
+
 		$this->set_current_fragment( $compiler, 'function_does_not_exists' );
 		$this->assertfalse( $compiler->get_function_content() );
+
 		$this->set_current_fragment( $compiler, array( $this, 'method_does_not_exist' ) );
 		$this->assertfalse( $compiler->get_function_content() );
 	}
 
 	/**
-	 * Test get_function_content() should return content from function.
+	 * Test get_function_content() should return content from a function.
 	 */
-	public function test_should_return_return_content_from_function() {
+	public function test_should_return_content_from_function() {
 		$fragment = 'beans_test_get_function_content_callback';
 		$compiler = new \_Beans_Compiler( array() );
 
@@ -55,7 +58,7 @@ class Tests_Beans_Compiler_Get_Function_Content extends Compiler_Test_Case {
 	/**
 	 * Test get_function_content() should return content from an object's method.
 	 */
-	public function test_should_return_return_content_from_method() {
+	public function test_should_return_content_from_method() {
 		$compiler = new \_Beans_Compiler( array() );
 
 		// Set up the mock.
