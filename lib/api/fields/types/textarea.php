cf7e2ab113231728d5d66b9679afecb9283e9fb0
hellofromtonya <hellofromtonya@knowthecode.io>
2018-02-21 11:35:52 -0600

    Made textarea field compliant & tested.

--- lib/api/fields/types/textarea.php
+++ lib/api/fields/types/textarea.php
@@ -1,28 +1,30 @@
 <?php
 /**
- * @package API\Fields\Types
+ * Render the textarea field type.
+ *
+ * @package Beans\Framework\API\Options
  */
 
 beans_add_smart_action( 'beans_field_textarea', 'beans_field_textarea' );
 /**
- * Echo textarea field type.
+ * Render the textarea field type.
  *
  * @since 1.0.0
  *
- * @param array $field {
+ * @param array $field      {
  *      For best practices, pass the array of data obtained using {@see beans_get_fields()}.
  *
- *      @type mixed  $value      The field value.
- *      @type string $name       The field name value.
- *      @type array  $attributes An array of attributes to add to the field. The array key defines the
- *            					 attribute name and the array value defines the attribute value. Default array.
- *      @type mixed  $default    The default value. Default false.
+ * @type mixed  $value      The field value.
+ * @type string $name       The field name value.
+ * @type array  $attributes An array of attributes to add to the field. The array key defines the attribute name and
+ *                          the array value defines the attribute value. Default array.
+ * @type mixed  $default    The default value. Default false.
  * }
  */
 function beans_field_textarea( $field ) {
-
-	?>
-	<textarea name="<?php echo esc_attr( $field['name'] ); ?>" <?php echo beans_esc_attributes( $field['attributes'] ); ?>><?php echo esc_textarea( $field['value'] ); ?></textarea>
-	<?php
-
+	printf( '<textarea name="%s" %s>%s</textarea>',
+		esc_attr( $field['name'] ),
+		beans_esc_attributes( $field['attributes'] ), // @codingStandardsIgnoreLine - WordPress.XSS.EscapeOutput.OutputNotEscaped - Escaping is handled in the function.
+		esc_textarea( $field['value'] )
+	);
 }
---
+++ tests/phpunit/integration/api/fields/types/beansFieldTextarea.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Tests for beans_field_textarea()
+ *
+ * @package Beans\Framework\Tests\Integration\API\Fields\Types
+ *
+ * @since   1.5.0
+ */
+
+namespace Beans\Framework\Tests\Integration\API\Fields\Types;
+
+use Beans\Framework\Tests\Integration\API\Fields\Includes\Fields_Test_Case;
+
+require_once dirname( __DIR__ ) . '/includes/class-fields-test-case.php';
+
+/**
+ * Class Tests_BeansFieldTextarea
+ *
+ * @package Beans\Framework\Tests\Integration\API\Fields
+ * @group   integration-tests
+ * @group   api
+ */
+class Tests_BeansFieldTextarea extends Fields_Test_Case {
+
+	/**
+	 * Prepares the test environment before each test.
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		// Load the field type.
+		require_once BEANS_THEME_DIR . '/lib/api/fields/types/textarea.php';
+	}
+
+	/**
+	 * Cleans up the test environment after each test.
+	 */
+	public function tearDown() {
+		parent::setUp();
+
+		beans_remove_action( 'beans_field_textarea', 'beans_field_textarea' );
+	}
+
+	/**
+	 * Test beans_field_textarea() should render the textarea field.
+	 */
+	public function test_should_render_text_field() {
+		$field = $this->merge_field_with_default( array(
+			'id'             => 'beans_textarea_test',
+			'type'           => 'textarea',
+			'default'        => '',
+		) );
+		$field['value'] = 'Testing the textarea field.';
+
+		ob_start();
+		beans_field_textarea( $field );
+		$html = ob_get_clean();
+
+		// Run the test.
+		$this->assertSame( '<textarea name="beans_fields[beans_textarea_test]" >Testing the textarea field.</textarea>', $html );
+	}
+
+	/**
+	 * Test beans_field_textarea() should render the textarea field with attributes when given.
+	 */
+	public function test_should_render_text_field_with_attributes_when_given() {
+		$field = $this->merge_field_with_default( array(
+			'id'             => 'beans_textarea_test',
+			'type'           => 'textarea',
+			'default'        => '',
+			'attributes'     => array(
+				'data-test' => 'foo',
+			),
+		) );
+		$field['value'] = 'Testing the textarea field with attributes.';
+
+		ob_start();
+		beans_field_textarea( $field );
+		$html = ob_get_clean();
+
+		// Run the test.
+		$this->assertSame( '<textarea name="beans_fields[beans_textarea_test]" data-test="foo">Testing the textarea field with attributes.</textarea>', $html );
+	}
+}
