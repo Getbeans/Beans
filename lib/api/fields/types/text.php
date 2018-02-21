5c73099fbede6a34a3c2a1a25b1f2e84ed6cce3f
hellofromtonya <hellofromtonya@knowthecode.io>
2018-02-21 11:34:26 -0600

    Made text type compliant + tested.

--- lib/api/fields/types/text.php
+++ lib/api/fields/types/text.php
@@ -1,6 +1,8 @@
 <?php
 /**
- * @package API\Fields\Types
+ * Renders the slider field type.
+ *
+ * @package Beans\Framework\API\Options
  */
 
 beans_add_smart_action( 'beans_field_text', 'beans_field_text' );
@@ -9,20 +11,20 @@ beans_add_smart_action( 'beans_field_text', 'beans_field_text' );
  *
  * @since 1.0.0
  *
- * @param array $field {
+ * @param array $field      {
  *      For best practices, pass the array of data obtained using {@see beans_get_fields()}.
  *
- *      @type mixed  $value      The field value.
- *      @type string $name       The field name value.
- *      @type array  $attributes An array of attributes to add to the field. The array key defines the
- *            					 attribute name and the array value defines the attribute value. Default array.
- *      @type mixed  $default    The default value. Default false.
+ * @type mixed  $value      The field value.
+ * @type string $name       The field name value.
+ * @type array  $attributes An array of attributes to add to the field. The array key defines the attribute name and
+ *                          the array value defines the attribute value. Default array.
+ * @type mixed  $default    The default value. Default false.
  * }
  */
 function beans_field_text( $field ) {
-
-	?>
-	<input type="text" name="<?php echo esc_attr( $field['name'] ); ?>" value="<?php echo esc_attr( $field['value'] ); ?>" <?php echo beans_esc_attributes( $field['attributes'] ); ?>>
-	<?php
-
+	printf( '<input type="text" name="%s" value="%s" %s>',
+		esc_attr( $field['name'] ),
+		esc_attr( $field['value'] ),
+		beans_esc_attributes( $field['attributes'] ) // @codingStandardsIgnoreLine - WordPress.XSS.EscapeOutput.OutputNotEscaped - Escaping is handled in the function.
+	);
 }
---
+++ tests/phpunit/unit/api/fields/types/beansFieldText.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Tests for beans_field_text()
+ *
+ * @package Beans\Framework\Tests\Unit\API\Fields\Types
+ *
+ * @since   1.5.0
+ */
+
+namespace Beans\Framework\Tests\Unit\API\Fields\Types;
+
+use Beans\Framework\Tests\Unit\API\Fields\Includes\Fields_Test_Case;
+
+require_once dirname( __DIR__ ) . '/includes/class-fields-test-case.php';
+
+/**
+ * Class Tests_BeansFieldText
+ *
+ * @package Beans\Framework\Tests\Unit\API\Fields
+ * @group   unit-tests
+ * @group   api
+ */
+class Tests_BeansFieldText extends Fields_Test_Case {
+
+	/**
+	 * Prepares the test environment before each test.
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		// Load the field type.
+		require_once BEANS_THEME_DIR . '/lib/api/fields/types/text.php';
+	}
+
+	/**
+	 * Cleans up the test environment after each test.
+	 */
+	public function tearDown() {
+		parent::setUp();
+
+		beans_remove_action( 'beans_field_text', 'beans_field_text' );
+	}
+
+	/**
+	 * Test beans_field_text() should render the text field.
+	 */
+	public function test_should_render_text_field() {
+		$field = $this->merge_field_with_default( array(
+			'id'             => 'beans_text_test',
+			'type'           => 'text',
+			'default'        => '',
+		) );
+		$field['value'] = 'Testing the text field.';
+
+		ob_start();
+		beans_field_text( $field );
+		$html = ob_get_clean();
+
+		// Run the test.
+		$this->assertSame( '<input type="text" name="beans_fields[beans_text_test]" value="Testing the text field." >', $html );
+	}
+
+	/**
+	 * Test beans_field_text() should render the text field with attributes when given.
+	 */
+	public function test_should_render_text_field_with_attributes_when_given() {
+		$field = $this->merge_field_with_default( array(
+			'id'             => 'beans_text_test',
+			'type'           => 'text',
+			'default'        => '',
+			'attributes'     => array(
+				'data-test' => 'foo',
+			),
+		) );
+		$field['value'] = 'Testing the text field with attributes.';
+
+		ob_start();
+		beans_field_text( $field );
+		$html = ob_get_clean();
+
+		// Run the test.
+		$this->assertSame( '<input type="text" name="beans_fields[beans_text_test]" value="Testing the text field with attributes." data-test="foo">', $html );
+
+	}
+}
