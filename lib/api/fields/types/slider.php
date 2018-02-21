84d4ca96efb3c04750ccd599d4cf12c6e36a6280
hellofromtonya <hellofromtonya@knowthecode.io>
2018-02-21 12:15:13 -0600

    Made slider field compliant + tested.

--- lib/api/fields/types/slider.php
+++ lib/api/fields/types/slider.php
@@ -1,6 +1,8 @@
 <?php
 /**
- * @package API\Fields\Types
+ * Renders the slider field type.
+ *
+ * @package Beans\Framework\API\Options
  */
 
 beans_add_smart_action( 'beans_field_enqueue_scripts_slider', 'beans_field_slider_assets' );
@@ -10,9 +12,7 @@ beans_add_smart_action( 'beans_field_enqueue_scripts_slider', 'beans_field_slide
  * @since 1.0.0
  */
 function beans_field_slider_assets() {
-
 	wp_enqueue_script( 'jquery-ui-slider' );
-
 }
 
 beans_add_smart_action( 'beans_field_slider', 'beans_field_slider' );
@@ -21,22 +21,21 @@ beans_add_smart_action( 'beans_field_slider', 'beans_field_slider' );
  *
  * @since 1.0.0
  *
- * @param array $field {
- *      For best practices, pass the array of data obtained using {@see beans_get_fields()}.
+ * @param array    $field      {
+ *                             For best practices, pass the array of data obtained using {@see beans_get_fields()}.
  *
- *      @type mixed  $value      The field value.
- *      @type string $name       The field name value.
- *      @type array  $attributes An array of attributes to add to the field. The array key defines the
- *            					 attribute name and the array value defines the attribute value. Default array.
- *      @type mixed  $default    The default value. Default false.
- *      @type string $min        The slider minimum value. Default 0.
- *      @type string $max        The slider maximum value. Default 100.
- *      @type string $interval   The slider interval. Default 1.
- *      @type string $unit       The slider unit. Default null.
+ * @type mixed     $value      The field's current value.
+ * @type string    $name       The field's "name" value.
+ * @type array     $attributes An array of attributes to add to the field. The array key defines the
+ *                                 attribute name and the array value defines the attribute value. Default array.
+ * @type int|float $default    The default value.
+ * @type string    $min        The slider's minimum value. Default 0.
+ * @type string    $max        The slider's maximum value. Default 100.
+ * @type string    $interval   The slider's interval. Default 1.
+ * @type string    $unit       The slider's units, which is displayed after the current value. Default null.
  * }
  */
 function beans_field_slider( $field ) {
-
 	$defaults = array(
 		'min'      => 0,
 		'max'      => 100,
@@ -44,19 +43,19 @@ function beans_field_slider( $field ) {
 		'unit'     => null,
 	);
 
-	$field	= array_merge( $defaults, $field );
+	$field = array_merge( $defaults, $field );
 
 	?>
 	<div class="bs-slider-wrap" slider_min="<?php echo (int) $field['min']; ?>" slider_max="<?php echo (int) $field['max']; ?>" slider_interval="<?php echo (int) $field['interval']; ?>">
 
 		<?php // Don't make this a hidden field to prevent triggering issues with wp_customise. ?>
-		<input type="text" value="<?php echo esc_attr( $field['value'] ); ?>" name="<?php echo esc_attr( $field['name'] ); ?>" <?php echo beans_esc_attributes( $field['attributes'] ); ?> style="display: none;"/>
+		<input type="text" value="<?php echo esc_attr( $field['value'] ); ?>" name="<?php echo esc_attr( $field['name'] ); ?>" style="display: none;" <?php echo beans_esc_attributes( $field['attributes'] ); ?>/><?php // @codingStandardsIgnoreLine - WordPress.XSS.EscapeOutput.OutputNotEscaped - Escaping is handled in the function. ?>
 
 	</div>
-	<span class="bs-slider-value"><?php echo $field['value']; ?></span>
+	<span class="bs-slider-value"><?php echo esc_html( $field['value'] ); ?></span>
 
 	<?php if ( $field['unit'] ) : ?>
-		<span class="bs-slider-unit"><?php echo $field['unit']; ?></span>
-	<?php endif;
-
+		<span class="bs-slider-unit"><?php echo esc_html( $field['unit'] ); ?></span>
+	<?php
+	endif;
 }
---
+++ tests/phpunit/integration/api/fields/types/beansFieldSlider.php
@@ -0,0 +1,168 @@
+<?php
+/**
+ * Tests for beans_field_slider()
+ *
+ * @package Beans\Framework\Tests\Integration\API\Fields\Types
+ *
+ * @since   1.5.0
+ */
+
+namespace Beans\Framework\Tests\Integration\API\Fields\Types;
+
+use Beans\Framework\Tests\Integration\API\Fields\Includes\Fields_Test_Case;
+
+require_once dirname( __DIR__ ) . '/includes/class-fields-test-case.php';
+
+/**
+ * Class Tests_BeansFieldSlider
+ *
+ * @package Beans\Framework\Tests\Integration\API\Fields
+ * @group   integration-tests
+ * @group   api
+ */
+class Tests_BeansFieldSlider extends Fields_Test_Case {
+
+	/**
+	 * Prepares the test environment before each test.
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		// Load the field type.
+		require_once BEANS_THEME_DIR . '/lib/api/fields/types/slider.php';
+	}
+
+	/**
+	 * Cleans up the test environment after each test.
+	 */
+	public function tearDown() {
+		parent::setUp();
+
+		beans_remove_action( 'beans_field_slider', 'beans_field_slider' );
+		beans_remove_action( 'beans_field_enqueue_scripts_slider', 'beans_field_enqueue_scripts_slider' );
+	}
+
+	/**
+	 * Test beans_field_slider() should render the slider field.
+	 */
+	public function test_should_render_slider_field() {
+		$field = $this->merge_field_with_default( array(
+			'id'          => 'beans_test_slider',
+			'label'       => 'Test Slider',
+			'description' => 'Testing the slider',
+			'type'        => 'slider',
+			'default'     => 0,
+			'min'         => 0,
+			'max'         => 100,
+			'interval'    => 1,
+		) );
+
+		ob_start();
+		beans_field_slider( $field );
+		$html = ob_get_clean();
+
+		$expected = <<<EOB
+<div class="bs-slider-wrap" slider_min="0" slider_max="100" slider_interval="1">
+    <input type="text" value="0" name="beans_fields[beans_test_slider]" style="display: none;" />
+</div>
+<span class="bs-slider-value">0</span>
+EOB;
+		// Run the test.
+		$this->assertSame( $this->format_the_html( $expected ), $this->format_the_html( $html ) );
+	}
+
+	/**
+	 * Test beans_field_slider() should render the slider field with unit when given.
+	 */
+	public function test_should_render_slider_field_with_unit_when_given() {
+		$field = $this->merge_field_with_default( array(
+			'id'          => 'beans_test_slider',
+			'label'       => 'Test Slider',
+			'description' => 'Testing the slider',
+			'type'        => 'slider',
+			'default'     => 0,
+			'min'         => 10,
+			'max'         => 100,
+			'interval'    => 5,
+			'unit'        => 'Number of beans',
+		) );
+
+		ob_start();
+		beans_field_slider( $field );
+		$html = ob_get_clean();
+
+		$expected = <<<EOB
+<div class="bs-slider-wrap" slider_min="10" slider_max="100" slider_interval="5">
+    <input type="text" value="0" name="beans_fields[beans_test_slider]" style="display: none;" />
+</div>
+<span class="bs-slider-value">0</span>
+<span class="bs-slider-unit">Number of beans</span>
+EOB;
+		// Run the test.
+		$this->assertSame( $this->format_the_html( $expected ), $this->format_the_html( $html ) );
+	}
+
+	/**
+	 * Test beans_field_slider() should render the slider with the current value.
+	 */
+	public function test_should_render_slider_with_current_value() {
+		$field          = $this->merge_field_with_default( array(
+			'id'          => 'beans_test_slider',
+			'label'       => 'Test Slider',
+			'description' => 'Testing the slider',
+			'type'        => 'slider',
+			'default'     => 0,
+			'min'         => 10,
+			'max'         => 100,
+			'interval'    => 5,
+		) );
+		$field['value'] = 15;
+
+		ob_start();
+		beans_field_slider( $field );
+		$html = ob_get_clean();
+
+		$expected = <<<EOB
+<div class="bs-slider-wrap" slider_min="10" slider_max="100" slider_interval="5">
+    <input type="text" value="15" name="beans_fields[beans_test_slider]" style="display: none;" />
+</div>
+<span class="bs-slider-value">15</span>
+EOB;
+		// Run the test.
+		$this->assertSame( $this->format_the_html( $expected ), $this->format_the_html( $html ) );
+	}
+
+	/**
+	 * Test beans_field_slider() should render the slider field with unit when given.
+	 */
+	public function test_should_render_slider_field_with_attributes_when_given() {
+		$field = $this->merge_field_with_default( array(
+			'id'          => 'beans_test_slider',
+			'label'       => 'Test Slider',
+			'description' => 'Testing the slider',
+			'type'        => 'slider',
+			'default'     => 1,
+			'min'         => 1,
+			'max'         => 20,
+			'interval'    => 1,
+			'unit'        => 'Number of beans',
+			'attributes'  => array(
+				'data-test' => 'foo',
+			),
+		) );
+
+		ob_start();
+		beans_field_slider( $field );
+		$html = ob_get_clean();
+
+		$expected = <<<EOB
+<div class="bs-slider-wrap" slider_min="1" slider_max="20" slider_interval="1">
+    <input type="text" value="1" name="beans_fields[beans_test_slider]" style="display: none;" data-test="foo"/>
+</div>
+<span class="bs-slider-value">1</span>
+<span class="bs-slider-unit">Number of beans</span>
+EOB;
+		// Run the test.
+		$this->assertSame( $this->format_the_html( $expected ), $this->format_the_html( $html ) );
+	}
+}
