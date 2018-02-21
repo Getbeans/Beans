0b23eed0877188f9331c36c876db34986602431c
hellofromtonya <hellofromtonya@knowthecode.io>
2018-02-21 11:38:19 -0600

    Made activation field compliant & tested.

--- lib/api/fields/types/activation.php
+++ lib/api/fields/types/activation.php
@@ -1,6 +1,8 @@
 <?php
 /**
- * @package API\Fields\Types
+ * Renders the activation field type.
+ *
+ * @package Beans\Framework\API\Options
  */
 
 beans_add_smart_action( 'beans_field_activation', 'beans_field_activation' );
@@ -9,23 +11,21 @@ beans_add_smart_action( 'beans_field_activation', 'beans_field_activation' );
  *
  * @since 1.0.0
  *
- * @param array $field {
+ * @param array $field       {
  *      For best practices, pass the array of data obtained using {@see beans_get_fields()}.
  *
- *      @type string $description The field description. The description can be truncated using <!--more-->
- *            					  as a delimiter. Default false.
- *      @type mixed  $value       The field value.
- *      @type string $name        The field name value.
- *      @type array  $attributes  An array of attributes to add to the field. The array key defines the
- *            					  attribute name and the array value defines the attribute value. Default array.
- *      @type mixed  $default     The default value. Default false.
+ * @type string $description The field description. The description can be truncated using <!--more--> as a delimiter.
+ *       Default false.
+ * @type mixed  $value       The field value.
+ * @type string $name        The field name value.
+ * @type array  $attributes  An array of attributes to add to the field. The array key defines the attribute name and
+ *       the array value defines the attribute value. Default array.
+ * @type mixed  $default     The default value. Default false.
  * }
  */
-function beans_field_activation( $field ) {
-
+function beans_field_activation( array $field ) {
 	?>
 	<input type="hidden" value="0" name="<?php echo esc_attr( $field['name'] ); ?>" />
-	<input type="checkbox" name="<?php echo esc_attr( $field['name'] ); ?>" value="1" <?php checked( $field['value'], 1 ); ?> <?php echo beans_esc_attributes( $field['attributes'] ); ?>/>
+	<input type="checkbox" name="<?php echo esc_attr( $field['name'] ); ?>" value="1"<?php checked( $field['value'], 1 ); ?> <?php echo beans_esc_attributes( $field['attributes'] ); ?>/><?php // @codingStandardsIgnoreLine - WordPress.XSS.EscapeOutput.OutputNotEscaped - Escaping is handled in the function. ?>
 	<?php
-
 }
