ac55fc6f3d2a15c61506b21dd63b245f080500b8
hellofromtonya <hellofromtonya@knowthecode.io>
2018-02-21 11:43:17 -0600

    Made checkbox field compliant + tested.

--- lib/api/fields/types/checkbox.php
+++ lib/api/fields/types/checkbox.php
@@ -1,6 +1,8 @@
 <?php
 /**
- * @package API\Fields\Types
+ * Renders the checkbox field type.
+ *
+ * @package Beans\Framework\API\Fields\Types
  */
 
 beans_add_smart_action( 'beans_field_checkbox', 'beans_field_checkbox' );
@@ -9,24 +11,24 @@ beans_add_smart_action( 'beans_field_checkbox', 'beans_field_checkbox' );
  *
  * @since 1.0.0
  *
- * @param array $field {
- *      For best practices, pass the array of data obtained using {@see beans_get_fields()}.
+ * @param array $field          {
+ *                              For best practices, pass the array of data obtained using {@see beans_get_fields()}.
  *
- *      @type mixed  $value          The field value.
- *      @type string $name           The field name value.
- *      @type array  $attributes     An array of attributes to add to the field. The array key defines the
- *            					     attribute name and the array value defines the attribute value. Default array.
- *      @type mixed  $default        The default value. Default false.
- *      @type string $checkbox_label The field checkbox label. Default 'Enable'.
+ * @type mixed  $value          The field value.
+ * @type string $name           The field name value.
+ * @type array  $attributes     An array of attributes to add to the field. The array key defines the attribute name
+ *       and the array value defines the attribute value. Default array.
+ * @type mixed  $default        The default value. Default false.
+ * @type string $checkbox_label The field checkbox label. Default 'Enable'.
  * }
  */
-function beans_field_checkbox( $field ) {
+function beans_field_checkbox( array $field ) {
+	$checkbox_label = beans_get( 'checkbox_label', $field, __( 'Enable', 'tm-beans' ) );
 
 	?>
-	<input type="hidden" value="0" name="<?php echo esc_attr( $field['name'] ); ?>" />
-	<input type="checkbox" name="<?php echo esc_attr( $field['name'] ); ?>" value="1" <?php checked( $field['value'], 1 ); ?> <?php echo beans_esc_attributes( $field['attributes'] ); ?>/>
-	<?php if ( $checkbox_label = beans_get( 'checkbox_label', $field, __( 'Enable', 'tm-beans' ) ) ) : ?>
-		<span class="bs-checkbox-label"><?php echo $checkbox_label; ?></span>
+    <input type="hidden" value="0" name="<?php echo esc_attr( $field['name'] ); ?>" />
+    <input type="checkbox" name="<?php echo esc_attr( $field['name'] ); ?>" value="1"<?php checked( $field['value'], 1 ); ?> <?php echo beans_esc_attributes( $field['attributes'] ); ?>/><?php // @codingStandardsIgnoreLine - WordPress.XSS.EscapeOutput.OutputNotEscaped - Escaping is handled in the function. ?>
+	<?php if ( $checkbox_label ) : ?>
+        <span class="bs-checkbox-label"><?php esc_html( $checkbox_label ); ?></span>
 	<?php endif;
-
 }
---
+++ tests/phpunit/unit/api/fields/types/beansFieldCheckbox.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * Tests for beans_field_checkbox()
+ *
+ * @package Beans\Framework\Tests\Unit\API\Fields\Types
+ *
+ * @since   1.5.0
+ */
+
+namespace Beans\Framework\Tests\Unit\API\Fields\Types;
+
+use Beans\Framework\Tests\Unit\API\Fields\Includes\Fields_Test_Case;
+
+require_once dirname( __DIR__ ) . '/includes/class-fields-test-case.php';
+
+/**
+ * Class Tests_BeansFieldCheckbox
+ *
+ * @package Beans\Framework\Tests\Unit\API\Fields
+ * @group   unit-tests
+ * @group   api
+ */
+class Tests_BeansFieldCheckbox extends Fields_Test_Case {
+
+	/**
+	 * Prepares the test environment before each test.
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		// Load the field type.
+		require_once BEANS_THEME_DIR . '/lib/api/fields/types/checkbox.php';
+	}
+
+	/**
+	 * Cleans up the test environment after each test.
+	 */
+	public function tearDown() {
+		parent::setUp();
+
+		beans_remove_action( 'beans_field_checkbox', 'beans_field_checkbox' );
+	}
+
+	/**
+	 * Test beans_field_checkbox() should render the checkbox with the label when given.
+	 */
+	public function test_should_render_a_checkbox_with_label_when_given() {
+		$field = $this->merge_field_with_default( array(
+			'id'             => 'beans_compile_all_styles',
+			'label'          => false,
+			'checkbox_label' => 'Compile all WordPress styles',
+			'type'           => 'checkbox',
+			'default'        => false,
+		) );
+
+		ob_start();
+		beans_field_checkbox( $field );
+		$html = ob_get_clean();
+
+		$expected = <<<EOB
+<input type="hidden" value="0" name="beans_fields[beans_compile_all_styles]" />
+<input type="checkbox" name="beans_fields[beans_compile_all_styles]" value="1" />
+<span class="bs-checkbox-label">Compile all WordPress styles</span>
+EOB;
+		// Run the test.
+		$this->assertSame( $this->format_the_html( $expected ), $this->format_the_html( $html ) );
+	}
+
+	/**
+	 * Test beans_field_checkbox() should render the checkbox with the default label when none is given.
+	 */
+	public function test_should_render_a_checkbox_with_default_label_when_none_is_given() {
+		$field = $this->merge_field_with_default( array(
+			'id'      => 'beans_compile_all_styles',
+			'type'    => 'checkbox',
+			'default' => false,
+		) );
+
+		ob_start();
+		beans_field_checkbox( $field );
+		$html = ob_get_clean();
+
+		$expected = <<<EOB
+<input type="hidden" value="0" name="beans_fields[beans_compile_all_styles]" />
+<input type="checkbox" name="beans_fields[beans_compile_all_styles]" value="1" />
+<span class="bs-checkbox-label">Enable</span>
+EOB;
+		// Run the test.
+		$this->assertSame( $this->format_the_html( $expected ), $this->format_the_html( $html ) );
+	}
+
+	/**
+	 * Test beans_field_checkbox() should render the checkbox with attributes when given.
+	 */
+	public function test_should_render_a_checkbox_with_attributes_when_given() {
+		$field = $this->merge_field_with_default( array(
+			'id'             => 'beans_compile_all_styles',
+			'checkbox_label' => 'Compile all WordPress styles',
+			'type'           => 'checkbox',
+			'default'        => false,
+			'attributes'     => array(
+				'data-test' => 'foo',
+			),
+		) );
+
+		ob_start();
+		beans_field_checkbox( $field );
+		$html = ob_get_clean();
+
+		$expected = <<<EOB
+<input type="hidden" value="0" name="beans_fields[beans_compile_all_styles]" />
+<input type="checkbox" name="beans_fields[beans_compile_all_styles]" value="1" data-test="foo"/>
+<span class="bs-checkbox-label">Compile all WordPress styles</span>
+EOB;
+		// Run the test.
+		$this->assertSame( $this->format_the_html( $expected ), $this->format_the_html( $html ) );
+	}
+}
