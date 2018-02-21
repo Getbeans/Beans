d70f1a7921f487341034bd7b6b77ac109a7c7751
hellofromtonya <hellofromtonya@knowthecode.io>
2018-02-21 12:00:44 -0600

    Made radio field compliant & tested.

--- lib/api/fields/types/radio.php
+++ lib/api/fields/types/radio.php
@@ -1,6 +1,8 @@
 <?php
 /**
- * @package API\Fields\Types
+ * Renders the radio field type.
+ *
+ * @package Beans\Framework\API\Fields\Types
  */
 
 beans_add_smart_action( 'beans_field_radio', 'beans_field_radio' );
@@ -9,46 +11,44 @@ beans_add_smart_action( 'beans_field_radio', 'beans_field_radio' );
  *
  * @since 1.0.0
  *
- * @param array $field {
- *      For best practices, pass the array of data obtained using {@see beans_get_fields()}.
+ * @param array $field      {
+ *                          For best practices, pass the array of data obtained using {@see beans_get_fields()}.
  *
- *      @type mixed  $value      The field value.
- *      @type string $name       The field name value.
- *      @type array  $attributes An array of attributes to add to the field. The array key defines the
- *            					 attribute name and the array value defines the attribute value. Default array.
- *      @type mixed  $default    The default value. Default false.
- *      @type array  $options    An array used to populate the radio options. The array key defines radio
- *            					 value and the array value defines the radio label or image path.
+ * @type mixed  $value      The field value.
+ * @type string $name       The field name value.
+ * @type array  $attributes An array of attributes to add to the field. The array key defines the attribute name and
+ *       the array value defines the attribute value. Default array.
+ * @type mixed  $default    The default value. Default false.
+ * @type array  $options    An array used to populate the radio options. The array key defines radio value and the
+ *       array value defines the radio label or image path.
  * }
  */
-function beans_field_radio( $field ) {
+function beans_field_radio( array $field ) {
 
 	if ( empty( $field['options'] ) ) {
 		return;
 	}
 
-	$field['default'] = isset( $checkbox['default'] ) ? $checkbox['default'] : key( $field['options'] );
+	$field['default'] = key( $field['options'] );
+	$extensions       = array( 'jpg', 'jpeg', 'jpe', 'gif', 'png', 'bmp', 'tif', 'tiff', 'ico' );
 
 	?>
 	<fieldset>
-		<?php $i = 0; foreach ( $field['options'] as $id => $radio ) :
-
-			$extensions = array( 'jpg', 'jpeg', 'jpe',  'gif',  'png',  'bmp',   'tif',  'tiff', 'ico' );
+		<?php
+		foreach ( $field['options'] as $id => $radio ) :
 			$has_image = in_array( beans_get( 'extension', pathinfo( $radio ) ), $extensions ) ? 'bs-has-image' : false;
-
 			?>
 			<label class="<?php echo esc_attr( $has_image ); ?>">
 				<?php if ( $has_image ) : ?>
-					<img src="<?php echo esc_url( $radio ); ?>"/>
+					<img src="<?php echo esc_url( $radio ); ?>" />
 				<?php endif; ?>
-				<input type="radio" name="<?php echo esc_attr( $field['name'] ); ?>" value="<?php echo esc_attr( $id ); ?>" <?php checked( $id, $field['value'], 1 ); ?> <?php echo beans_esc_attributes( $field['attributes'] ); ?>/>
+                <input type="radio" name="<?php echo esc_attr( $field['name'] ); ?>" value="<?php echo esc_attr( $id ); ?>"<?php checked( $id, $field['value'], 1 ); ?> <?php echo beans_esc_attributes( $field['attributes'] ); ?>/><?php // @codingStandardsIgnoreLine phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped - Escaping is handled in the function. ?>
 				<?php if ( ! $has_image ) : ?>
-					<?php echo $radio; ?>
+					<?php echo wp_kses_post( $radio ); ?>
 				<?php endif; ?>
 			</label>
 
-		<?php $i++; endforeach; ?>
+		<?php endforeach; ?>
 	</fieldset>
 	<?php
-
 }
---
+++ tests/phpunit/integration/api/fields/types/beansFieldRadio.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Tests for beans_field_radio()
+ *
+ * @package Beans\Framework\Tests\Integration\API\Fields\Types
+ *
+ * @since   1.5.0
+ */
+
+namespace Beans\Framework\Tests\Integration\API\Fields\Types;
+
+use Beans\Framework\Tests\Integration\API\Fields\Includes\Fields_Test_Case;
+
+require_once dirname( __DIR__ ) . '/includes/class-fields-test-case.php';
+
+/**
+ * Class Tests_BeansFieldRadio
+ *
+ * @package Beans\Framework\Tests\Integration\API\Fields
+ * @group   integration-tests
+ * @group   api
+ */
+class Tests_BeansFieldRadio extends Fields_Test_Case {
+
+	/**
+	 * Prepares the test environment before each test.
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		// Load the field type.
+		require_once BEANS_THEME_DIR . '/lib/api/fields/types/radio.php';
+	}
+
+	/**
+	 * Cleans up the test environment after each test.
+	 */
+	public function tearDown() {
+		parent::setUp();
+
+		beans_remove_action( 'beans_field_radio', 'beans_field_radio' );
+	}
+
+	/**
+	 * Test beans_field_radio() should render a radio field with image options.
+	 */
+	public function test_should_render_a_radio_field_with_image_options() {
+		$field = $this->merge_field_with_default( array(
+			'id'      => 'beans_layout',
+			'label'   => 'Layout',
+			'type'    => 'radio',
+			'default' => 'default_fallback',
+			'options' => array(
+				'default_fallback' => 'Use Default Layout',
+				'c'                => 'http://example.com/images/layouts/c.png',
+				'c_sp'             => 'http://example.com/images/layouts/c_sp.png',
+				'sp_c'             => 'http://example.com/images/layouts/sp_c.png',
+			),
+		) );
+
+		ob_start();
+		beans_field_radio( $field );
+		$html = ob_get_clean();
+
+		$expected = <<<EOB
+<fieldset>
+    <label class="">
+        <input type="radio" name="beans_fields[beans_layout]" value="default_fallback" checked='checked' />
+        Use Default Layout</label>
+    <label class="bs-has-image">
+        <img src="http://example.com/images/layouts/c.png" />
+        <input type="radio" name="beans_fields[beans_layout]" value="c" />
+    </label>
+    <label class="bs-has-image">
+        <img src="http://example.com/images/layouts/c_sp.png" />
+        <input type="radio" name="beans_fields[beans_layout]" value="c_sp" />
+    </label>
+    <label class="bs-has-image">
+        <img src="http://example.com/images/layouts/sp_c.png" />
+        <input type="radio" name="beans_fields[beans_layout]" value="sp_c" />
+    </label>
+</fieldset>
+EOB;
+
+		// Run the test.
+		$this->assertSame( $this->format_the_html( $expected ), $this->format_the_html( $html ) );
+	}
+}
