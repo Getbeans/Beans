5ebd551b74963cabaf543dac65c62a16631a24f1
Christoph Herr <info@christophherr.com>
2018-02-25 15:36:40 -0500

    Fix typo in 404.php

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
  * This core file should only be overwritten via your child theme.
  *
  * We strongly recommend to read the Beans documentation to find out more about
- * how to customize Beans theme.
+ * how to customize the Beans theme.
  *
  * @author Beans
  * @link   http://www.getbeans.io
