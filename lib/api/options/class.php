366b5d73365f9ff92bb12087f470c8ac46f7fd38
Christoph Herr <christophherr@users.noreply.github.com>
2018-02-25 22:16:43 -0500

    Made lib\api\options WPCS compliant PR #127

--- lib/api/options/class.php
+++ lib/api/options/class.php
@@ -1,67 +1,89 @@
 <?php
 /**
+ * This class provides the means to handle the Beans Options workflow.
+ *
+ * @package Beans\Framework\API\Options
+ *
+ * @since 1.0.0
+ */
+
+/**
  * Handle the Beans Options workflow.
  *
+ * @since 1.0.0
  * @ignore
+ * @access private
  *
- * @package API\Options
+ * @package Beans\Framework\API\Options
  */
 final class _Beans_Options {
 
 	/**
 	 * Metabox arguments.
 	 *
-	 * @type array
+	 * @var array
 	 */
 	private $args = array();
 
 	/**
 	 * Form submission status.
 	 *
-	 * @type bool
+	 * @var bool
 	 */
 	private $success = false;
 
 	/**
-	 * Fields section.
+	 * Field section.
 	 *
-	 * @type string
+	 * @var string
 	 */
 	private $section;
 
 	/**
 	 * Register options.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $section Section of the field.
+	 * @param array  $args Arguments of the option.
+	 *
+	 * @return void
 	 */
 	public function register( $section, $args ) {
-
 		$defaults = array(
 			'title'   => __( 'Undefined', 'tm-beans' ),
 			'context' => 'normal',
 		);
 
 		$this->section = $section;
-		$this->args = array_merge( $defaults, $args );
+		$this->args    = array_merge( $defaults, $args );
 
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_assets' ) );
 
 		$this->register_metabox();
-
 	}
 
 	/**
 	 * Enqueue assets.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
 	 */
 	public function enqueue_assets() {
-
 		wp_enqueue_script( 'postbox' );
-
 	}
 
 	/**
 	 * Register the Metabox.
+	 *
+	 * @since 1.0.0
+	 * @ignore
+	 * @access private
+	 *
+	 * @return void
 	 */
 	private function register_metabox() {
-
 		add_meta_box(
 			$this->section,
 			$this->args['title'],
@@ -70,32 +92,41 @@ final class _Beans_Options {
 			$this->args['context'],
 			'default'
 		);
-
 	}
 
 	/**
 	 * Metabox content.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
 	 */
 	public function metabox_content() {
 
 		foreach ( beans_get_fields( 'option', $this->section ) as $field ) {
 			beans_field( $field );
 		}
-
 	}
 
 	/**
 	 * Page content.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param int $page Page ID.
+	 *
+	 * @return void
 	 */
 	public function page( $page ) {
-
 		global $wp_meta_boxes;
 
-		if ( ! $boxes = beans_get( $page, $wp_meta_boxes ) ) {
+		$boxes = beans_get( $page, $wp_meta_boxes );
+
+		if ( ! $boxes ) {
 			return;
 		}
 
-		// Only add column class if there is more than 1 metaboxes.
+		// Only add a column class if there is more than 1 metabox.
 		$column_class = beans_get( 'column', $boxes, array() ) ? ' column' : false;
 
 		// Set page data which will be used by the postbox.
@@ -119,32 +150,34 @@ final class _Beans_Options {
 			</p>
 		</form>
 		<?php
-
 	}
 
 	/**
 	 * Form actions.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
 	 */
 	public function actions() {
 
 		if ( beans_post( 'beans_save_options' ) ) {
-
 			$this->save();
 			add_action( 'admin_notices', array( $this, 'save_notices' ) );
-
 		}
 
 		if ( beans_post( 'beans_reset_options' ) ) {
-
 			$this->reset();
 			add_action( 'admin_notices', array( $this, 'reset_notices' ) );
-
 		}
-
 	}
 
 	/**
 	 * Save options.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return bool
 	 */
 	private function save() {
 
@@ -152,7 +185,9 @@ final class _Beans_Options {
 			return false;
 		}
 
-		if ( ! ( $fields = beans_post( 'beans_fields' ) ) ) {
+		$fields = beans_post( 'beans_fields' );
+
+		if ( ! ( $fields ) ) {
 			return false;
 		}
 
@@ -161,11 +196,14 @@ final class _Beans_Options {
 		}
 
 		$this->success = true;
-
 	}
 
 	/**
 	 * Reset options.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return bool
 	 */
 	private function reset() {
 
@@ -173,7 +211,9 @@ final class _Beans_Options {
 			return false;
 		}
 
-		if ( ! ( $fields = beans_post( 'beans_fields' ) ) ) {
+		$fields = beans_post( 'beans_fields' );
+
+		if ( ! ( $fields ) ) {
 			return false;
 		}
 
@@ -182,24 +222,27 @@ final class _Beans_Options {
 		}
 
 		$this->success = true;
-
 	}
 
 	/**
 	 * Save notice content.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
 	 */
 	public function save_notices() {
 
 		if ( $this->success ) {
 			?>
 			<div id="message" class="updated">
-				<p><?php _e( 'Settings saved successfully!', 'tm-beans' ); ?></p>
+				<p><?php esc_html_e( 'Settings saved successfully!', 'tm-beans' ); ?></p>
 			</div>
 			<?php
 		} else {
 			?>
 			<div id="message" class="error">
-				<p><?php _e( 'Settings could not be saved, please try again.', 'tm-beans' ); ?></p>
+				<p><?php esc_html_e( 'Settings could not be saved, please try again.', 'tm-beans' ); ?></p>
 			</div>
 			<?php
 		}
@@ -207,22 +250,25 @@ final class _Beans_Options {
 
 	/**
 	 * Reset notice content.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
 	 */
 	public function reset_notices() {
 
 		if ( $this->success ) {
 			?>
 			<div id="message" class="updated">
-				<p><?php _e( 'Settings reset successfully!', 'tm-beans' ); ?></p>
+				<p><?php esc_html_e( 'Settings reset successfully!', 'tm-beans' ); ?></p>
 			</div>
 			<?php
 		} else {
 			?>
 			<div id="message" class="error">
-				<p><?php _e( 'Settings could not be reset, please try again.', 'tm-beans' ); ?></p>
+				<p><?php esc_html_e( 'Settings could not be reset, please try again.', 'tm-beans' ); ?></p>
 			</div>
 			<?php
 		}
-
 	}
 }
--- lib/api/options/functions.php
+++ lib/api/options/functions.php
@@ -1,8 +1,10 @@
 <?php
 /**
- * The Beans Options component extends the Beans Fields and make it easy to add fields to a WordPress options page.
+ * The Beans Options component extends the Beans Fields API and makes it easy to add fields to a WordPress options page.
  *
- * @package API\Options
+ * @package Beans\Framework\API\Options
+ *
+ * @since 1.0.0
  */
 
 /**
@@ -15,30 +17,30 @@
  * @param array  $fields {
  *      Array of fields to register.
  *
- * 		@type string $id          A unique id used for the field. This id will also be used to save the value in
- * 		      					  the database.
- * 		@type string $type 		  The type of field to use. Please refer to the Beans core field types for more
- * 		      					  information. Custom field types are accepted here.
- *      @type string $label 	  The field label. Default false.
+ *      @type string $id          A unique id used for the field. This id will also be used to save the value in
+ *                                the database.
+ *      @type string $type        The type of field to use. Please refer to the Beans core field types for more
+ *                                information. Custom field types are accepted here.
+ *      @type string $label       The field label. Default false.
  *      @type string $description The field description. The description can be truncated using <!--more-->
- *            					  as a delimiter. Default false.
+ *                                as a delimiter. Default false.
  *      @type array  $attributes  An array of attributes to add to the field. The array key defines the
- *            					  attribute name and the array value defines the attribute value. Default array.
+ *                                attribute name and the array value defines the attribute value. Default array.
  *      @type mixed  $default     The default field value. Default false.
  *      @type array  $fields      Must only be used for 'group' field type. The array arguments are similar to the
- *            					  {@see beans_register_fields()} $fields arguments.
+ *                                {@see beans_register_fields()} $fields arguments.
  *      @type bool   $db_group    Must only be used for 'group' field types. Defines whether the group of fields
- *            					  registered should be saved as a group in the database or as individual
- *            					  entries. Default false.
+ *                                registered should be saved as a group in the database or as individual
+ *                                entries. Default false.
  * }
  * @param string $menu_slug The menu slug used by fields.
  * @param string $section   A section id to define the group of fields.
  * @param array  $args {
  *      Optional. Array of arguments used to register the fields.
  *
- * 		@type string $title   The metabox Title. Default 'Undefined'.
- * 		@type string $context Where on the page where the metabox should be shown
- * 		      				  ('normal', 'column'). Default 'normal'.
+ *      @type string $title   The metabox Title. Default 'Undefined'.
+ *      @type string $context Where on the page where the metabox should be shown
+ *                            ('normal', 'column'). Default 'normal'.
  * }
  *
  * @return bool True on success, false on failure.
@@ -78,13 +80,12 @@ function beans_register_options( array $fields, $menu_slug, $section, $args = ar
 	}
 
 	// Load the class only if this function is called to prevent unnecessary memory usage.
-	require_once( BEANS_API_PATH . 'options/class.php' );
+	require_once BEANS_API_PATH . 'options/class.php';
 
 	$class = new _Beans_Options();
 	$class->register( $section, $args );
 
 	return true;
-
 }
 
 /**
@@ -95,6 +96,8 @@ function beans_register_options( array $fields, $menu_slug, $section, $args = ar
  * @since 1.0.0
  *
  * @param array $menu_slug The menu slug used to register the options.
+ *
+ * @return bool
  */
 function beans_options( $menu_slug ) {
 
@@ -104,14 +107,17 @@ function beans_options( $menu_slug ) {
 
 	$class = new _Beans_Options();
 	$class->page( $menu_slug );
-
 }
 
 add_action( 'wp_loaded', '_beans_options_page_actions' );
 /**
  * Fires the options form actions.
  *
+ * @since 1.0.0
  * @ignore
+ * @access private
+ *
+ * @return void
  */
 function _beans_options_page_actions() {
 
@@ -120,9 +126,8 @@ function _beans_options_page_actions() {
 	}
 
 	// Load the class only if this function is called to prevent unnecessary memory usage.
-	require_once( BEANS_API_PATH . 'options/class.php' );
+	require_once BEANS_API_PATH . 'options/class.php';
 
 	$class = new _Beans_Options();
 	$class->actions();
-
 }
