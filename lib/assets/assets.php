ffb8208688237fae81b1c4180a51f637d010e6ff
Christoph Herr <christophherr@users.noreply.github.com>
2018-02-28 00:46:49 -0500

    Made lib\assets\assets.php WPCS compliant (#141)

--- lib/assets/assets.php
+++ lib/assets/assets.php
@@ -2,19 +2,22 @@
 /**
  * Add Beans assets.
  *
- * @package Assets
+ * @package Beans\Framework\Assets
+ *
+ * @since   1.0.0
  */
 
 beans_add_smart_action( 'beans_uikit_enqueue_scripts', 'beans_enqueue_uikit_components', 5 );
 /**
- * Enqueue UIKit components and Beans style.
+ * Enqueue UIkit components and Beans style.
  *
  * Beans style is enqueued with the UIKit components to have access to UIKit LESS variables.
  *
  * @since 1.0.0
+ *
+ * @return void
  */
 function beans_enqueue_uikit_components() {
-
 	$core = array(
 		'base',
 		'block',
@@ -42,20 +45,19 @@ function beans_enqueue_uikit_components() {
 
 	beans_uikit_enqueue_components( $core, 'core', false );
 
-	// Include uikit default theme.
+	// Include UIkit default theme.
 	beans_uikit_enqueue_theme( 'default' );
 
-	// Enqueue uikit overwrite theme folder.
+	// Enqueue UIkit overwrite theme folder.
 	beans_uikit_enqueue_theme( 'beans', BEANS_ASSETS_PATH . 'less/uikit-overwrite' );
 
-	// Add the theme style as a uikit fragment to have access to all the variables.
+	// Add the theme style as a UIkit fragment to have access to all the variables.
 	beans_compiler_add_fragment( 'uikit', BEANS_ASSETS_PATH . 'less/style.less', 'less' );
 
-	// Add the theme default style as a uikit fragment only if the theme supports it.
+	// Add the theme default style as a UIkit fragment only if the theme supports it.
 	if ( current_theme_supports( 'beans-default-styling' ) ) {
 		beans_compiler_add_fragment( 'uikit', BEANS_ASSETS_PATH . 'less/default.less', 'less' );
 	}
-
 }
 
 beans_add_smart_action( 'wp_enqueue_scripts', 'beans_enqueue_assets', 5 );
@@ -63,13 +65,14 @@ beans_add_smart_action( 'wp_enqueue_scripts', 'beans_enqueue_assets', 5 );
  * Enqueue Beans assets.
  *
  * @since 1.0.0
+ *
+ * @return void
  */
 function beans_enqueue_assets() {
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
-
 }
 
 beans_add_smart_action( 'after_setup_theme', 'beans_add_editor_assets' );
@@ -77,9 +80,9 @@ beans_add_smart_action( 'after_setup_theme', 'beans_add_editor_assets' );
  * Add Beans editor assets.
  *
  * @since 1.2.5
+ *
+ * @return void
  */
 function beans_add_editor_assets() {
-
 	add_editor_style( BEANS_ASSETS_URL . 'css/editor' . BEANS_MIN_CSS . '.css' );
-
 }
