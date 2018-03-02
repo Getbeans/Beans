20dba6ba7de602fc855d7ae99468dd6b8f478c6f
Christoph Herr <christophherr@users.noreply.github.com>
2018-03-02 12:42:48 -0500

    Made lib\templates\structure WPCS compliant (#140)

--- lib/templates/structure/sidebar-primary.php
+++ lib/templates/structure/sidebar-primary.php
@@ -2,15 +2,21 @@
 /**
  * Echo the primary sidebar structural markup. It also calls the primary sidebar action hooks.
  *
- * @package Structure\Primary_Sidebar
+ * @package Beans\Framework\Templates\Structure
+ *
+ * @since   1.0.0
  */
 
-beans_open_markup_e( 'beans_sidebar_primary', 'aside', array(
-	'class'     => 'tm-secondary ' . beans_get_layout_class( 'sidebar_primary' ), // Automatically escaped.
-	'role'      => 'complementary',
-	'itemscope' => 'itemscope',
-	'itemtype'  => 'http://schema.org/WPSideBar',
-) );
+beans_open_markup_e(
+	'beans_sidebar_primary',
+	'aside',
+	array(
+		'class'     => 'tm-secondary ' . beans_get_layout_class( 'sidebar_primary' ), // Automatically escaped.
+		'role'      => 'complementary',
+		'itemscope' => 'itemscope',
+		'itemtype'  => 'http://schema.org/WPSideBar',
+	)
+);
 
 	/**
 	 * Fires in the primary sidebar.
--- lib/templates/structure/widget-area.php
+++ lib/templates/structure/widget-area.php
@@ -3,25 +3,38 @@
  * Echo the widget area and widget loop structural markup. It also calls the widget area and widget loop
  * action hooks.
  *
- * @package Structure\Widget_Area
+ * @package Beans\Framework\Templates\Structure
+ *
+ * @since   1.0.0
  */
 
 // This includes everything added to wp hooks before the widgets.
-echo beans_get_widget_area( 'before_widgets' );
+echo beans_get_widget_area( 'before_widgets' ); // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped -- Widget area has to be echoed.
 
-	if ( 'grid' == beans_get_widget_area( 'beans_type' ) ) {
-		beans_open_markup_e( 'beans_widget_area_grid' . _beans_widget_area_subfilters(), 'div', array( 'class' => 'uk-grid', 'data-uk-grid-margin' => '' ) );
+	// phpcs:disable Generic.WhiteSpace.ScopeIndent -- Code structure mirrors HTML markup.
+	if ( 'grid' === beans_get_widget_area( 'beans_type' ) ) {
+		beans_open_markup_e(
+			'beans_widget_area_grid' . _beans_widget_area_subfilters(),
+			'div',
+			array(
+				'class'               => 'uk-grid',
+				'data-uk-grid-margin' => '',
+			)
+		);
 	}
 
-	if ( 'offcanvas' == beans_get_widget_area( 'beans_type' ) ) {
+	if ( 'offcanvas' === beans_get_widget_area( 'beans_type' ) ) {
 
-		beans_open_markup_e( 'beans_widget_area_offcanvas_wrap' . _beans_widget_area_subfilters(), 'div', array(
-			'id'    => beans_get_widget_area( 'id' ), // Automatically escaped.
-			'class' => 'uk-offcanvas',
-		) );
+		beans_open_markup_e(
+			'beans_widget_area_offcanvas_wrap' . _beans_widget_area_subfilters(),
+			'div',
+			array(
+				'id'    => beans_get_widget_area( 'id' ), // Automatically escaped.
+				'class' => 'uk-offcanvas',
+			)
+		);
 
 			beans_open_markup_e( 'beans_widget_area_offcanvas_bar' . _beans_widget_area_subfilters(), 'div', array( 'class' => 'uk-offcanvas-bar' ) );
-
 	}
 
 		// Widgets.
@@ -36,9 +49,10 @@ echo beans_get_widget_area( 'before_widgets' );
 			 */
 			do_action( 'beans_before_widgets_loop' );
 
-				while ( beans_have_widgets() ) : beans_setup_widget();
+				while ( beans_have_widgets() ) :
+					beans_setup_widget();
 
-					if ( 'grid' == beans_get_widget_area( 'beans_type' ) ) {
+					if ( 'grid' === beans_get_widget_area( 'beans_type' ) ) {
 						beans_open_markup_e( 'beans_widget_grid' . _beans_widget_subfilters(), 'div', beans_widget_shortcodes( 'class=uk-width-medium-1-{count}' ) );
 					}
 
@@ -53,10 +67,9 @@ echo beans_get_widget_area( 'before_widgets' );
 
 						beans_close_markup_e( 'beans_widget_panel' . _beans_widget_subfilters(), 'div' );
 
-					if ( 'grid' == beans_get_widget_area( 'beans_type' ) ) {
+					if ( 'grid' === beans_get_widget_area( 'beans_type' ) ) {
 						beans_close_markup_e( 'beans_widget_grid' . _beans_widget_subfilters(), 'div' );
 					}
-
 				endwhile;
 
 			/**
@@ -67,7 +80,6 @@ echo beans_get_widget_area( 'before_widgets' );
 			 * @since 1.0.0
 			 */
 			do_action( 'beans_after_widgets_loop' );
-
 		else :
 
 			/**
@@ -76,20 +88,20 @@ echo beans_get_widget_area( 'before_widgets' );
 			 * @since 1.0.0
 			 */
 			do_action( 'beans_no_widget' );
-
 		endif;
 
-	if ( 'offcanvas' == beans_get_widget_area( 'beans_type' ) ) {
+	if ( 'offcanvas' === beans_get_widget_area( 'beans_type' ) ) {
 
 			beans_close_markup_e( 'beans_widget_area_offcanvas_bar' . _beans_widget_area_subfilters(), 'div' );
 
 		beans_close_markup_e( 'beans_widget_area_offcanvas_wrap' . _beans_widget_area_subfilters(), 'div' );
-
 	}
 
-	if ( 'grid' == beans_get_widget_area( 'beans_type' ) ) {
+	if ( 'grid' === beans_get_widget_area( 'beans_type' ) ) {
 		beans_close_markup_e( 'beans_widget_area_grid' . _beans_widget_area_subfilters(), 'div' );
 	}
 
 // This includes everything added to wp hooks after the widgets.
-echo beans_get_widget_area( 'after_widgets' );
+echo beans_get_widget_area( 'after_widgets' ); // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped -- Widget area has to be echoed.
+
+// phpcs:enable Generic.WhiteSpace.ScopeIndent -- Code structure mirrors HTML markup.
