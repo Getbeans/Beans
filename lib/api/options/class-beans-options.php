f6adee1d875875443c8e34a8041cf6a26cb16061
hellofromtonya <hellofromtonya@knowthecode.io>
2018-03-11 11:41:00 -0500

    Moved HTML into a view file. Separation of concerns.

--- lib/api/options/class-beans-options.php
+++ lib/api/options/class-beans-options.php
@@ -132,27 +132,7 @@ final class _Beans_Options {
 		// Only add a column class if there is more than 1 metabox.
 		$column_class = beans_get( 'column', $boxes, array() ) ? ' column' : false;
 
-		// Set page data which will be used by the postbox.
-		?>
-		<form action="" method="post" class="bs-options" data-page="<?php esc_attr_e( beans_get( 'page' ) ); ?>">
-			<?php wp_nonce_field( 'closedpostboxes', 'closedpostboxesnonce', false ); ?>
-			<?php wp_nonce_field( 'meta-box-order', 'meta-box-order-nonce', false ); ?>
-			<input type="hidden" name="beans_options_nonce" value="<?php esc_attr_e( wp_create_nonce( 'beans_options_nonce' ) ); ?>" />
-			<div class="metabox-holder<?php echo esc_attr( $column_class ); ?>">
-				<?php
-				do_meta_boxes( $page, 'normal', null );
-
-				if ( $column_class ) {
-					do_meta_boxes( $page, 'column', null );
-				}
-				?>
-			</div>
-			<p class="bs-options-form-actions">
-				<input type="submit" name="beans_save_options" value="<?php esc_attr_e( 'Save', 'tm-beans' ); ?>" class="button-primary">
-				<input type="submit" name="beans_reset_options" value="<?php esc_attr_e( 'Reset', 'tm-beans' ); ?>" class="button-secondary">
-			</p>
-		</form>
-		<?php
+		include dirname( __FILE__ ) . '/views/page.php';
 	}
 
 	/**
