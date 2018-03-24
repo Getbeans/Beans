99bab4ac13342a7ff02aaea0a60eb6a7d29b5f8f
hellofromtonya <hellofromtonya@knowthecode.io>
2018-03-23 20:16:16 -0500

    Mocked function in _beans_set_action.

--- tests/phpunit/unit/api/actions/beansSetAction.php
+++ tests/phpunit/unit/api/actions/beansSetAction.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Tests for _beans_set_action()
+ * Tests for _beans_set_action().
  *
  * @package Beans\Framework\Tests\Unit\API\Actions
  *
@@ -10,6 +10,7 @@
 namespace Beans\Framework\Tests\Unit\API\Actions;
 
 use Beans\Framework\Tests\Unit\API\Actions\Includes\Actions_Test_Case;
+use Brain\Monkey;
 
 require_once __DIR__ . '/includes/class-actions-test-case.php';
 
@@ -42,6 +43,11 @@ class Tests_BeansSetAction extends Actions_Test_Case {
 				// Before we start, check that the action is not set.
 				$this->assertArrayNotHasKey( $beans_id, $_beans_registered_actions[ $status ] );
 
+				Monkey\Functions\expect( '_beans_get_action' )
+					->once()
+					->with( $beans_id, $status )
+					->andReturn( false ); // Return false as the action is not stored.
+
 				// Now do the tests.
 				$this->assertSame( $action, _beans_set_action( $beans_id, $action, $status ) );
 				$this->assertArrayHasKey( $beans_id, $_beans_registered_actions[ $status ] );
@@ -70,6 +76,12 @@ class Tests_BeansSetAction extends Actions_Test_Case {
 				// Register the original action.
 				$this->assertSame( $action, _beans_set_action( $beans_id, $action, $status ) );
 
+				// Simulate getting the original action from the container.
+				Monkey\Functions\expect( '_beans_get_action' )
+					->once()
+					->with( $beans_id, $status )
+					->andReturn( $action );
+
 				// Now test that it does not overwrite the previously registered action.
 				$this->assertSame( $action, _beans_set_action( $beans_id, $new_action, $status ) );
 				$this->assertSame( $action, $_beans_registered_actions[ $status ][ $beans_id ] );
@@ -96,8 +108,15 @@ class Tests_BeansSetAction extends Actions_Test_Case {
 			// Test each status.
 			foreach ( $this->statuses as $status ) {
 				// Register the original action.
+				Monkey\Functions\expect( '_beans_get_action' )
+					->once()
+					->with( $beans_id, $status )
+					->andReturn( false );
 				$this->assertSame( $action, _beans_set_action( $beans_id, $action, $status ) );
 				$this->assertSame( $action, $_beans_registered_actions[ $status ][ $beans_id ] );
+
+				// Check that _beans_get_action() does not get called.
+				Monkey\Functions\expect( '_beans_get_action' )->never();
 
 				// Now test that it does overwrite the previously registered action.
 				$this->assertSame( $new_action, _beans_set_action( $beans_id, $new_action, $status, true ) );
