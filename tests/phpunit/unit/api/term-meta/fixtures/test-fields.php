1ff541496223f5eacd94ee33d6278f70e69764c4
Caspar Green <cg@caspar.green>
2018-04-30 11:54:51 -0400

    Add term-meta field test data fixture

---
+++ tests/phpunit/unit/api/term-meta/fixtures/test-fields.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Test data for the Term Meta unit tests.
+ *
+ * @package Beans\Framework\Tests\Unit\API\Term_Meta\Fixtures
+ *
+ * @since   1.5.0
+ */
+
+return array(
+	array(
+		'id'    => 'field_id',
+		'type'  => 'radio',
+		'label' => 'Field Label',
+	),
+);
