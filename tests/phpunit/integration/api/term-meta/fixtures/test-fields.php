311add419b28cae5f8a5f8664ad7e2431d458d9d
Caspar Green <cg@caspar.green>
2018-05-01 09:22:14 -0400

    Add int tests term_meta_test_case and fixtures

---
+++ tests/phpunit/integration/api/term-meta/fixtures/test-fields.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ *  Test field data for use with render_fields method tests.
+ *
+ * @package Beans\Framework\Tests\Integration\API\Term_Meta\Fixtures
+ *
+ * @since 1.5.0
+ */
+
+return array(
+	'fields'  => array(
+		array(
+			'id'      => 'beans_layout_test',
+			'label'   => 'Layout',
+			'type'    => 'radio',
+			'default' => 'default_fallback',
+			'options' => array(
+				'default_fallback' => 'Use Default Layout',
+				'c'                => BEANS_THEME_DIR . 'lib/admin/assets/images/layouts/c.png',
+				'c_sp'             => BEANS_THEME_DIR . 'lib/admin/assets/images/layouts/c_sp.png',
+				'sp_c'             => BEANS_THEME_DIR . 'lib/admin/assets/images/layouts/sp_c.png',
+			),
+		),
+		array(
+			'id'             => 'beans_checkbox_test',
+			'label'          => false,
+			'checkbox_label' => 'Enable the checkbox test',
+			'type'           => 'checkbox',
+			'default'        => false,
+		),
+		array(
+			'id'      => 'beans_text_test',
+			'type'    => 'text',
+			'default' => 'Testing the text field.',
+		),
+	),
+	'context' => 'tests',
+	'section' => 'tm-beans',
+);
