bff3f1cd6f43dacb9d80061618c08f96a49a4081
hellofromtonya <hellofromtonya@knowthecode.io>
2018-05-24 11:32:43 -0500

    Renamed and commented tests to make intent more clear.
    The tests are checking that when a invalid file is given but it has a
    valid basename...Beans loads its internal structure.

--- tests/phpunit/integration/api/template/beansLoadDefaultTemplate.php
+++ tests/phpunit/integration/api/template/beansLoadDefaultTemplate.php
@@ -32,19 +32,34 @@ class Tests_BeansLoadDefaultTemplate extends Test_Case {
 	}
 
 	/**
-	 * Test beans_load_default_template() should return true after loading the given structure.
+	 * Test beans_load_default_template() should load the default structure when the given file does not exist, but its
+	 * basename is a Beans' structure.
 	 */
-	public function test_should_return_true_after_loading_structure() {
-		// Check with an invalid path to the structure.
+	public function test_should_load_default_structure_when_given_file_does_not_exist_but_basename_is_beans_structure() {
+		// Check with a relative invalid path.
 		$file = '/path/to/structure/content.php';
+
+		// Check that the given file does not exist.
+		$this->assertFileNotExists( $file );
+
+		// Check that 'content.php' does exist in Beans.
 		$this->assertFileExists( BEANS_STRUCTURE_PATH . basename( $file ) );
+
+		// Check that it renders.
 		ob_start();
 		$this->assertTrue( beans_load_default_template( $file ) );
 		$this->assertStringStartsWith( '<div class="tm-content"', ob_get_clean() );
 
-		// Check with an absolute path to the structure.
-		$file = __DIR__ . 'fixtures/structure/header.php';
+		// Check with an invalid absolute path.
+		$file = __DIR__ . '/fixtures/structure/header.php';
+
+		// Check that the given file does not exist.
+		$this->assertFileNotExists( $file );
+
+		// Check that 'header.php' does exist in Beans.
 		$this->assertFileExists( BEANS_STRUCTURE_PATH . basename( $file ) );
+
+		// Check that it renders.
 		ob_start();
 		$this->assertTrue( beans_load_default_template( $file ) );
 		$this->assertStringStartsWith( '<!DOCTYPE html>', ob_get_clean() );
