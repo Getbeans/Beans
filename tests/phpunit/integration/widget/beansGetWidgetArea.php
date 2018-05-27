fb6d61c9c26aa6d04c351f5a0a92afa99b5bdcfc
Caspar Green <cg@caspar.green>
2018-05-27 14:32:17 -0400

    Fix in test for beans_get_widget_area()

--- tests/phpunit/integration/widget/beansGetWidgetArea.php
+++ tests/phpunit/integration/widget/beansGetWidgetArea.php
@@ -26,7 +26,10 @@ class Tests_BeansGetWidgetArea extends Beans_Widget_Test_Case {
 	 * Test beans_get_widget_area() should return false when widget area data is not found.
 	 */
 	public function test_should_return_false_when_widget_area_data_not_found() {
+		// Test for when needle is given.
 		$this->assertFalse( beans_get_widget_area( 'bogus-needle' ) );
+
+		// Test for when needle is not given.
 		$this->assertFalse( beans_get_widget_area() );
 	}
 
@@ -34,10 +37,8 @@ class Tests_BeansGetWidgetArea extends Beans_Widget_Test_Case {
 	 * Test beans_get_widget_area() should return all widget area data when needle is not specified.
 	 */
 	public function test_should_return_all_data_when_needle_not_specified() {
-		global $_beans_widget_area;
-
-		register_sidebar( array( 'id' => 'beans_test_sidebar', 'name' => 'Beans Test Sidebar') );
-		_beans_setup_widget_area( 'beans_test_sidebar' );
+		register_sidebar( array( 'id' => 'test_sidebar', 'name' => 'Test Sidebar') );
+		_beans_setup_widget_area( 'test_sidebar' );
 
 		$this->assertSame( $this->get_expected_sidebar_data(), beans_get_widget_area() );
 	}
@@ -46,18 +47,19 @@ class Tests_BeansGetWidgetArea extends Beans_Widget_Test_Case {
 	 * Test beans_get_widget_area() should return specific widget data when a needle is specified.
 	 */
 	public function test_should_return_specific_widget_data_when_needle_specified() {
-		global $_beans_widget_area;
+		register_sidebar( array( 'id' => 'test_sidebar', 'name' => 'Test Sidebar') );
+		_beans_setup_widget_area( 'test_sidebar' );
 
-		register_sidebar( array( 'id' => 'beans_test_sidebar', 'name' => 'Beans Test Sidebar') );
-		_beans_setup_widget_area( 'beans_test_sidebar' );
-
-		$this->assertEquals( 'beans_test_sidebar', beans_get_widget_area( 'id' ) );
+		$this->assertEquals( 'test_sidebar', beans_get_widget_area( 'id' ) );
 	}
 
+	/**
+	 * Return an array of expected sidebar data.
+	 */
 	protected function get_expected_sidebar_data() {
 		return array(
-			'name'           => 'Beans Test Sidebar',
-			'id'             => 'beans_test_sidebar',
+			'name'           => 'Test Sidebar',
+			'id'             => 'test_sidebar',
 			'description'    => '',
 			'class'          => '',
 			'before_widget'  => '<!--widget-%1$s-->',
