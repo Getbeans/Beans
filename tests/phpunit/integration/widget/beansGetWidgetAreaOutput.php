fad31fcbd183f9dac140f6a9a836d1ab092449e4
Caspar Green <cg@caspar.green>
2018-05-27 14:29:46 -0400

    Fix int test for beans_get_widget_area_output()

--- tests/phpunit/integration/widget/beansGetWidgetAreaOutput.php
+++ tests/phpunit/integration/widget/beansGetWidgetAreaOutput.php
@@ -33,7 +33,17 @@ class Tests_BeansGetWidgetAreaOutput extends Beans_Widget_Test_Case {
 	 * Test beans_get_widget_area_output() should return output when a widget area is registered.
 	 */
 	public function test_should_return_widget_output_when_widget_area_is_registered() {
-		$this->assertEquals( $this->get_expected_output(), beans_get_widget_area_output( 'sidebar_primary' ) );
+		global $wp_registered_sidebars;
+
+		beans_register_widget_area( array(
+			'id'   => 'test_sidebar',
+			'name' => 'Test Sidebar',
+		) );
+		$this->add_test_widget_to_test_sidebar();
+		$this->assertSame(
+			$this->format_the_html( $this->get_expected_output() ),
+			$this->format_the_html( beans_get_widget_area_output( 'test_sidebar' ) )
+		);
 	}
 
 	/**
@@ -41,16 +51,7 @@ class Tests_BeansGetWidgetAreaOutput extends Beans_Widget_Test_Case {
 	 */
 	protected function get_expected_output() {
 		return <<<OUTPUT
-<div class="tm-widget uk-panel widget_search search-2"><div ><form class="uk-form uk-form-icon uk-form-icon-flip uk-width-1-1" method="get" action="http://example.org/" role="search"><input class="uk-width-1-1" type="search" placeholder="Search" value="" name="s"/><span class="uk-icon-search" aria-hidden="true"></span></form></div></div><div class="tm-widget uk-panel widget_recent-comments recent-comments-2"><h3 class="uk-panel-title">Recent Comments</h3><div class="uk-list"><ul id="recentcomments"></ul></div></div><div class="tm-widget uk-panel widget_archives archives-2"><h3 class="uk-panel-title">Archives</h3><div class="uk-list">		<ul>
-				</ul>
-		</div></div><div class="tm-widget uk-panel widget_categories categories-2"><h3 class="uk-panel-title">Categories</h3><div class="uk-list">		<ul>
-<li class="cat-item-none">No categories</li>		</ul>
-</div></div><div class="tm-widget uk-panel widget_meta meta-2"><h3 class="uk-panel-title">Meta</h3><div class="uk-list">			<ul>
-						<li><a href="http://example.org/wp-login.php">Log in</a></li>
-			<li><a href="http://example.org/?feed=rss2">Entries <abbr title="Really Simple Syndication">RSS</abbr></a></li>
-			<li><a href="http://example.org/?feed=comments-rss2">Comments <abbr title="Really Simple Syndication">RSS</abbr></a></li>
-			<li><a href="https://wordpress.org/" title="Powered by WordPress, state-of-the-art semantic personal publishing platform.">WordPress.org</a></li>			</ul>
-			</div></div>
+		<div class="tm-widget uk-panel widget_text text-2"></div>
 OUTPUT;
 	}
 }
