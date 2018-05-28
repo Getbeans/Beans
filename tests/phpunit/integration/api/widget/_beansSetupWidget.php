445fb38699bf6b2686673eaea25cc7fc21ef50a3
Caspar Green <cg@caspar.green>
2018-05-28 15:58:34 -0400

    Add integration tests for 3 functions
    beans_setup_widget()
    beans_widget_area_shortcodes()
    _beans_setup_widget()

---
+++ tests/phpunit/integration/api/widget/_beansSetupWidget.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Tests for _beans_prepare_widget_data()
+ *
+ * @package Beans\Framework\Tests\Integration\API\Widget
+ *
+ * @since   1.5.0
+ */
+
+namespace Beans\Framework\Tests\Integration\API\Widget;
+
+use Beans\Framework\Tests\Integration\API\Widget\Includes\Beans_Widget_Test_Case;
+
+require_once dirname( __FILE__ ) . '/includes/class-beans-widget-test-case.php';
+
+/**
+ * Class Tests__BeansPrepareWidgetData
+ *
+ * @package Beans\Framework\Tests\Integration\API\Widget
+ * @group   api
+ * @group   api-widget
+ */
+class Tests_BeansPrepareWidgetData extends Beans_Widget_Test_Case {
+
+}
---
+++ tests/phpunit/integration/api/widget/beansSetupWidget.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Tests for beans_setup_widget()
+ *
+ * @package Beans\Framework\Tests\Integration\API\Widget
+ *
+ * @since   1.5.0
+ */
+
+namespace Beans\Framework\Tests\Integration\API\Widget;
+
+use Beans\Framework\Tests\Integration\API\Widget\Includes\Beans_Widget_Test_Case;
+
+require_once dirname( __FILE__ ) . '/includes/class-beans-widget-test-case.php';
+
+/**
+ * Class Tests_BeansSetupWidget
+ *
+ * @package Beans\Framework\Tests\Integration\API\Widget
+ * @group   api
+ * @group   api-widget
+ */
+class Tests_BeansSetupWidget extends Beans_Widget_Test_Case {
+
+	/**
+	 * Test beans_setup_widget() should return false when the widget ID can't be found.
+	 */
+	public function test_should_return_false_when_widget_id_not_found() {
+		global $_beans_widget_area;
+
+		beans_register_widget_area( array( 'id' => 'test_sidebar' ) );
+		$this->add_test_widget_to_test_sidebar();
+
+		_beans_setup_widget_area( 'test_sidebar' );
+
+		// Advance the widget counter to a non-existent widget.
+		$_beans_widget_area['current_widget'] = 1;
+		$this->assertFalse( beans_setup_widget() );
+	}
+
+	/**
+	 * Test beans_setup_widget() should return advance widget pointer and true when a widget ID is found.
+	 */
+	public function test_should_return_advance_widget_pointer_and_return_true_when_widget_id_is_found() {
+		global $_beans_widget_area;
+
+		beans_register_widget_area( array( 'id' => 'test_sidebar' ) );
+		$this->add_test_widget_to_test_sidebar();
+		_beans_setup_widget_area( 'test_sidebar' );
+
+		// Check widget pointer is at 0.
+		$this->assertEquals( 0, $_beans_widget_area['current_widget'] );
+
+		// Run test.
+		$this->assertTrue( beans_setup_widget() );
+
+		// Verify widget pointer has advanced to 1.
+		$this->assertEquals( 1, $_beans_widget_area['current_widget'] );
+	}
+}
