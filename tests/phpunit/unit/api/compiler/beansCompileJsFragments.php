e99fc9fee746793128e4b88e9119c51d6e598cc5
hellofromtonya <hellofromtonya@knowthecode.io>
2018-06-18 17:48:47 -0500

    Added tests for beans_compile_js_fragments().

---
+++ tests/phpunit/unit/api/compiler/beansCompileJsFragments.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Tests for beans_compile_js_fragments()
+ *
+ * @package Beans\Framework\Tests\Unit\API\Compiler
+ *
+ * @since   1.5.0
+ */
+
+namespace Beans\Framework\Tests\Unit\API\Compiler;
+
+use Beans\Framework\Tests\Unit\API\Compiler\Includes\Compiler_Test_Case;
+
+require_once __DIR__ . '/includes/class-compiler-test-case.php';
+
+/**
+ * Class Test_BeansCompileJsFragments
+ *
+ * @package Beans\Framework\Tests\Unit\API\Compiler
+ * @group   api
+ * @group   api-compiler
+ */
+class Test_BeansCompileJsFragments extends Compiler_Test_Case {
+
+	/**
+	 * Test beans_compile_js_fragments() should return false when no fragments are passed to it.
+	 */
+	public function test_should_return_false_when_fragments_are_passed_to_it() {
+		$this->assertFalse( beans_compile_js_fragments( 'foo', '' ) );
+		$this->assertFalse( beans_compile_js_fragments( 'foo', [] ) );
+		$this->assertFalse( beans_compile_js_fragments( 'foo', [], [
+			'dependencies' => [],
+			'in_footer'    => true,
+		] ) );
+	}
+}
